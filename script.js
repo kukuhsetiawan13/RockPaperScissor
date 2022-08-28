0092b12a02454c20c5dabdd53cbcd82ddb4849f5
kukuhsetiawan13 <study@kukuh.anonaddy.me>
2022-08-28 15:32:54 +0700

    Finished script.js with complete functionality of simple Rcok, Paper, Scissors game.

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
 function getComputerChoice() {
-   let randomized = Math.floor(Math.random()*3 + 1);
-   if (randomized === 1) {
-    return "rock";
-   } else if (randomized === 2) {
-    return "paper";
-   } else if (randomized === 3){
-    return "scissor";
-   }
-} 
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
-
-    if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("Both choosed rock, it's a tie!");
-    } else if (playerSelection === "rock" && computerSelection === "scissor") {
-        console.log("Rock beats scissor, you won!");
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("The rock is wrapped by the paper, sorry you lose!");
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You wrapped the rock, you won!");
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log("Both choosed paper, it's a tie!")
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
-        console.log("Ooops the enemy uses its scissor, you lose!");
-    } else if (playerSelection === "scissor" && computerSelection === "rock") {
-        console.log("you were smashed by the rock");
-    } else if (playerSelection === "scissor" && computerSelection === "paper") {
-        console.log("Congrats you cut the paper!");
-    } else if (playerSelection === "scissor" && computerSelection === "scissor") {
-        console.log("A scissor can't beat a scissor, it's a tie!");
+    let random = Math.floor(Math.random() * 3 +1);
+    switch (random) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3: 
+            return "scissors"
+            break;
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+
+function game() {
+    for (let i=1; i<=5; i++) {
+
+        do {
+            playerSelection = prompt("Please choose either rock, paper, or scissors: ");
+            playerSelection = playerSelection.toLowerCase();
+        } while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors");
+
+        let computerSelection = getComputerChoice();
+
+        function playRound(playerSelection, ComputerSelection) {
+             if (playerSelection === "rock" && computerSelection === "rock") {
+                return "Both choosed rock, it's a tie!";
+            } else if (playerSelection === "rock" && computerSelection === "scissors") {
+                return "Rock beats scissor, you won!";
+            } else if (playerSelection === "rock" && computerSelection === "paper") {
+                return "The rock is wrapped by the paper, sorry you lose!";
+            } else if (playerSelection === "paper" && computerSelection === "rock") {
+                return "You wrapped the rock, you won!";
+            } else if (playerSelection === "paper" && computerSelection === "paper") {
+                return "Both choosed paper, it's a tie!";
+            } else if (playerSelection === "paper" && computerSelection === "scissors") {
+                return"Ooops the enemy uses its scissor, you lose!";
+            } else if (playerSelection === "scissors" && computerSelection === "rock") {
+                return "you were smashed by the rock";
+            } else if (playerSelection === "scissors" && computerSelection === "paper") {
+                return "Congrats you cut the paper!";
+            } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+                return "A scissor can't beat a scissor, it's a tie!";
+            }
+        }
+
+        console.log(playRound(playerSelection, computerSelection));
+
+
+    }
+    
+}
+
+
+
+
+/*
+function playRound(playerSelection, ComputerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "Both choosed rock, it's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Rock beats scissor, you won!";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "The rock is wrapped by the paper, sorry you lose!";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You wrapped the rock, you won!";
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "Both choosed paper, it's a tie!";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return"Ooops the enemy uses its scissor, you lose!";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "you were smashed by the rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Congrats you cut the paper!";
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        return "A scissor can't beat a scissor, it's a tie!";
+    }
+}
+
+let playerSelection = "PaPER";
+let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+*/
