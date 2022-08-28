cdbe9634ca731ac13ec3e175247a07d16bcd7b53
kukuhsetiawan13 <study@kukuh.anonaddy.me>
2022-08-28 15:48:33 +0700

    Uploaded final version

--- script.js
+++ script.js
@@ -25,24 +25,34 @@ function game() {
         let computerSelection = getComputerChoice();
 
         function playRound(playerSelection, ComputerSelection) {
-             if (playerSelection === "rock" && computerSelection === "rock") {
-                return "Both choosed rock, it's a tie!";
-            } else if (playerSelection === "rock" && computerSelection === "scissors") {
-                return "Rock beats scissor, you won!";
-            } else if (playerSelection === "rock" && computerSelection === "paper") {
-                return "The rock is wrapped by the paper, sorry you lose!";
-            } else if (playerSelection === "paper" && computerSelection === "rock") {
-                return "You wrapped the rock, you won!";
-            } else if (playerSelection === "paper" && computerSelection === "paper") {
-                return "Both choosed paper, it's a tie!";
-            } else if (playerSelection === "paper" && computerSelection === "scissors") {
-                return"Ooops the enemy uses its scissor, you lose!";
-            } else if (playerSelection === "scissors" && computerSelection === "rock") {
-                return "you were smashed by the rock";
-            } else if (playerSelection === "scissors" && computerSelection === "paper") {
-                return "Congrats you cut the paper!";
-            } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-                return "A scissor can't beat a scissor, it's a tie!";
+            switch(true) {
+                case (playerSelection === "rock" && computerSelection === "rock"):
+                    return "Both choosed rock, it's a tie!";
+                    break;
+                case (playerSelection === "rock" && computerSelection === "scissors"):
+                    return "Rock beats scissor, you won!";
+                    break;
+                case (playerSelection === "rock" && computerSelection === "paper"):
+                    return "The rock is wrapped by the paper, sorry you lose!";
+                    break;
+                case (playerSelection === "paper" && computerSelection === "rock"):
+                    return "You wrapped the rock, you won!";
+                    break;
+                case (playerSelection === "paper" && computerSelection === "paper"):
+                    return "Both choosed paper, it's a tie!";
+                    break;
+                case (playerSelection === "paper" && computerSelection === "scissors"):
+                    return "Ooops the enemy uses its scissor, you lose!";
+                    break;
+                case (playerSelection === "scissors" && computerSelection === "rock"):
+                    return "you were smashed by the rock";
+                    break;
+                case (playerSelection === "scissors" && computerSelection === "paper"):
+                    return "Congrats you cut the paper!";
+                    break;
+                case (playerSelection === "scissors" && computerSelection === "scissors"):
+                    return "A scissor can't beat a scissor, it's a tie!";
+                    break;
             }
         }
 
@@ -56,7 +66,7 @@ function game() {
 
 
 
-/*
+/* IF INSTEAD SWITCH
 function playRound(playerSelection, ComputerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
@@ -85,3 +95,4 @@ let playerSelection = "PaPER";
 let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 */
+
